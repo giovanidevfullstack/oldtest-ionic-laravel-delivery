86e6ba0d25e1107c828f2c7cc2bc176f475913d0
Giovani Souza <shark.tec@hotmail.com>
2017-02-07 09:34:09 -0200

    geopos deliveryman

--- ionic/www/js/controllers/deliveryman/DeliverymanViewOrderCtrl.js
+++ ionic/www/js/controllers/deliveryman/DeliverymanViewOrderCtrl.js
@@ -1,7 +1,9 @@
 angular.module('starter.controllers')
     .controller('DeliverymanViewOrderCtrl',[
-        '$scope','DeliverymanOrderService','$ionicLoading','$stateParams',
-        function ($scope, DeliverymanOrderService, $ionicLoading, $stateParams) {
+        '$scope','DeliverymanOrderService','$ionicLoading','$stateParams','$cordovaGeolocation','$ionicPopup',
+        function ($scope, DeliverymanOrderService, $ionicLoading, $stateParams, $cordovaGeolocation, $ionicPopup) {
+            var watch;
+
             $scope.order = {};
 
             $ionicLoading.show({
@@ -15,9 +17,42 @@ angular.module('starter.controllers')
                 $scope.order = data.data;
                 $ionicLoading.hide();
             },function (error) {
-                console.debug(error);
                 $ionicLoading.hide();
             });
+
+            $scope.goToDelivery = function () {
+                $ionicPopup.alert({
+                   title: 'Alerta',
+                   template: 'Parar localização?'
+                }).then(function () {
+                    stopWatcPosition();
+                });
+
+                DeliverymanOrderService.updateStatus({id: $stateParams.id}, {status: 1}, function () {
+                   var watchOptions = {
+                       timeout: 3000,
+                       enableHighAccuracy: false
+                   };
+
+                   watch = $cordovaGeolocation.watchPosition(watchOptions);
+                   watch.then(null,
+                       function (responseError) {
+                           //error
+                       },
+                       function (position) {
+                           DeliverymanOrderService.geo({id: $stateParams.id},{
+                              lat:  position.coords.latitude,
+                              long: position.coords.longitude
+                           });
+                       });
+                });
+            };
+            
+            function stopWatcPosition() {
+                if(watch && typeof watch == 'object' && watch.hasOwnProperty('watchID')){
+                    $cordovaGeolocation.clearWatch(watch.watchID);
+                }
+            }
         }
     ]);
 
